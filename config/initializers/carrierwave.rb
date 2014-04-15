28df05afba976a96fdfd0ae2bac910657387b93a
crayfln <jeff.wge@gmail.com>
2014-04-15 16:47:30 -0700

    sadf

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,23 +1,22 @@
 CarrierWave.configure do |config|
   config.fog_credentials = {
     :provider               => 'AWS',                          # required
-    :aws_access_key_id      => ENV['AWS_ACCESS_KEY'],          # required
-    :aws_secret_access_key  => ENV['AWS_SECRET_KEY'],          # required
-    :region                 => ENV['AWS_REGION']              # optional, defaults to 'us-east-1'
-    # :host                   => 's3.example.com',             # optional, defaults to nil
-    # :endpoint               => 'http://s3-us-west-2.amazonaws.com' # optional, defaults to nil
+    :aws_access_key_id      => 'AKIAIZ4KL3JP4IRV6UBA',          # required
+    :aws_secret_access_key  => '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',          # required
+    :region                 => 'us-west-2'              # optional, defaults to 'us-east-1'
+    #:host                   => 's3.example.com',             # optional, defaults to nil
+    #:endpoint               => 'http://s3-us-west-2.amazonaws.com' # optional, defaults to nil
   }
+  config.fog_directory  = 'homeforgrandma-production'                       # required
+  config.fog_public     = true                                    # optional, defaults to true
+  config.fog_attributes = {'Cache-Control'=>'max-age=315576000'}  # optional, defaults to {}
 
-  if Rails.env.test? || Rails.env.development?
+
+  # For testing, upload files to local `tmp` folder.
+  if Rails.env.development?
     config.storage = :file
-    config.enable_processing = false
-    config.root = "#{Rails.root}/tmp/uploads/#{DateTime.now.to_f}.#{rand(999)}.#{rand(999)}"
   else
     config.storage = :fog
   end
-
-  config.cache_dir = "#{Rails.root}/tmp/uploads/#{DateTime.now.to_f}.#{rand(999)}.#{rand(999)}"
-  config.fog_directory  = ENV['AWS_BUCKET']
-  config.fog_public     = false
-  config.fog_attributes = {'Cache-Control'=>'max-age=315576000'}
 end
+
