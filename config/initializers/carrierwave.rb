e57a5c3d9c7caf72186a52849c4acf87897de343
crayfln <jeff.wge@gmail.com>
2014-04-15 16:53:55 -0700

    sadf

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,13 +1,13 @@
 CarrierWave.configure do |config|
   config.fog_credentials = {
     :provider               => 'AWS',                          # required
-    :aws_access_key_id      => 'AKIAIZ4KL3JP4IRV6UBA',          # required
-    :aws_secret_access_key  => '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',          # required
-    :region                 => 'us-west-2'              # optional, defaults to 'us-east-1'
-    #:host                   => 's3.example.com',             # optional, defaults to nil
-    #:endpoint               => 'http://s3-us-west-2.amazonaws.com' # optional, defaults to nil
+    :aws_access_key_id      => ENV['AWS_ACCESS_KEY'],          # required
+    :aws_secret_access_key  => ENV['AWS_SECRET_KEY'],          # required
+    :region                 => ENV['AWS_REGION']              # optional, defaults to 'us-east-1'
+    # :host                   => 's3.example.com',             # optional, defaults to nil
+    # :endpoint               => 'http://s3-us-west-2.amazonaws.com' # optional, defaults to nil
   }
-  config.fog_directory  = 'homeforgrandma-production'                       # required
+  config.fog_directory  = ENV['AWS_BUCKET']                       # required
   config.fog_public     = true                                    # optional, defaults to true
   config.fog_attributes = {'Cache-Control'=>'max-age=315576000'}  # optional, defaults to {}
 
@@ -19,4 +19,3 @@ CarrierWave.configure do |config|
     config.storage = :fog
   end
 end
-
